88ecae736c0fe17f4668527f78bdf9644bc03711
Halidi <halidikijuru109@gmail.com>
2023-11-13 11:13:01 +0300

    Lesson 2: Dart variable

---
+++ var.dart
@@ -0,0 +1,27 @@
+void main(){
+    // variable 
+
+    //string
+     var name = "Halidi";
+    //  String name = "Halidi Hamidu kijuru";
+
+    //  interger
+
+    var x = 41;
+
+    // int x = 41;
+
+    // Dynamic 
+    dynamic firstname ="Halidi hamidu kijuru";
+
+    // const and final 
+    const String fullName = "Halidi hamidu";
+
+    final String nickName = "J";
+
+    // Declaring and not using 
+
+    var myName;
+    myName = "Halidi hamidu";
+    print(myName);
+}
