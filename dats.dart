17fd34fbe8e5d8e19f383b5751f323b1c9139876
Halidi <halidikijuru109@gmail.com>
2023-11-13 11:42:57 +0300

    Lesson 3 Dart Datatypes

---
+++ dats.dart
@@ -0,0 +1,24 @@
+void main(){
+    // Data type
+    // Number, String , Booleans, List, Maps, Dynamic
+
+
+    // Strings
+
+    String firstName = "Halidi";
+    print("String : $firstName");
+
+    // integer
+
+    int myNum = 27;
+    print("Integer : $myNum");
+
+
+    // Doubles 
+    double otherNum = 3.1;
+    print("Double: $otherNum");
+
+    // dynamic 
+    dynamic fullName = 'Halidi Hamidu kijuru';
+    print("Dynamic : $fullName");
+}
