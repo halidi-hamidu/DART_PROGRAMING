f3e9f5747865943b07b8287e165cc4a03d1541a0
Halidi <halidikijuru109@gmail.com>
2023-11-13 15:38:56 +0300

    Lesson 6: Dart Loops

---
+++ loops.dart
@@ -0,0 +1,22 @@
+void main(){
+    // for loop
+
+    var num = 5;
+    for (var i =num ; i>= 1; i--){
+        print("The value of i is: $i");
+    }
+
+    //  for in Loop
+    var names = ['halidi', 'hamidu', 'kijuru'];
+    for (var name in names){
+        print("the member of list are : $name");
+    }
+
+
+    // while loop
+     while (num >= 1){
+        print(num);
+
+        num--;
+     }
+}
