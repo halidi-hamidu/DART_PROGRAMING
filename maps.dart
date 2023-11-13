524dcba1261ebb2904b8eeeef3072407b77bdc73
Halidi <halidikijuru109@gmail.com>
2023-11-13 15:26:56 +0300

    Lesson 5: Dart Map or dictionary

---
+++ maps.dart
@@ -0,0 +1,17 @@
+void main(){
+    // maps key/value pair
+
+    var topping = {
+        "john": "Ugali", 
+        "Halidi": "Mishkaki na chipsi",
+        "Kay": "she like only pizas"
+        };
+
+    print(topping);
+
+    print(topping.values);
+    print(topping.keys);
+    print(topping.length);
+    print(topping.remove('john'));
+    print(topping);
+}
