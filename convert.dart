34bc92b4f09edb8b1c95b99c56c25433c1b036c0
Halidi <halidikijuru109@gmail.com>
2023-11-14 11:27:23 +0300

    lesson 11: Dart convert Datatypes

---
+++ convert.dart
@@ -0,0 +1,29 @@
+void main(){
+    // Convert String , Int and Doubles
+    // String To Int
+
+
+    var a,b,c;
+
+    a=40;
+    b="1";
+    c = a + int.parse(b);
+    print("$a + $b = $c");
+
+
+    // String To Double
+    var d, e,f;
+    d = 40;
+    e = "0.1";
+    f = d + double.parse(e);
+    print("$d + $e = $f");
+
+    // int to String   
+    var g,h,i;
+
+    g = 40;
+    h = '1';
+    i = g.toString() + h;
+    print("$g + $h = $i");
+}
+
